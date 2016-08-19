25d3ec86c1dc3df98ab71b83092cc083abff59b9
Charlie DeTar <cfd@media.mit.edu>
2016-08-19 12:17:41 -0600

    Version 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2016, Charlie DeTar'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.2'
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags.
 release = '0.0.1'
 
